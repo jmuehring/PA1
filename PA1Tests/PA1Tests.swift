6ebf96c7b395086d7cbaa245790aced664f36a8c
jmuehring3 <jmuehring3@gatech.edu>
2025-01-28 03:15:43 -0500

    Initial Commit

---
+++ PA1Tests/PA1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  PA1Tests.swift
+//  PA1Tests
+//
+//  Created by Joshua Muehring on 1/28/25.
+//
+
+import Testing
+@testable import PA1
+
+struct PA1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
